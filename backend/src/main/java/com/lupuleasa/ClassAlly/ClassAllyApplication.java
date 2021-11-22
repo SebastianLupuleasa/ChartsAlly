156f1e9d560057ea294080214b97545d8358acca
Sebastian Lupuleasa <sebastian.lupuleasa01@e-uvt.ro>
2021-11-22 15:04:02 +0200

    first commit

---
+++ backend/src/main/java/com/lupuleasa/ClassAlly/ClassAllyApplication.java
@@ -0,0 +1,13 @@
+package com.lupuleasa.ClassAlly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassAllyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassAllyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lupuleasa/ClassAlly/ClassAllyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lupuleasa.ClassAlly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassAllyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
